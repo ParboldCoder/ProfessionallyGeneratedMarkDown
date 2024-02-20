d00fafda6df9db05cf4a57e98da437957b9cc51c
ParboldCoder <darren.a.buck@gmail.com>
2024-02-20 18:58:35 +0000

    added questions to js file

--- index.js
+++ index.js
@@ -5,8 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of your project:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Provide installation instructions:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide usage information:"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Provide contribution guidelines:"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide test instructions:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license for your project:",
+        choices: ["MIT", "GNU GPLv3", "Apache License 2.0", "ISC", "None"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
 ];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
